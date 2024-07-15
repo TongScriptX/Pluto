f4b908dfd9b7ecf5b6bad4e960f6a4c63b72b4d5
Pluto <158065504+TongScriptX@users.noreply.github.com>
2024-07-15 13:56:29 +0800

    Update Pluto.lua

--- Pluto/Pluto.lua
+++ Pluto/Pluto.lua
@@ -1,16 +1,9 @@
-local NotificationHolder =
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+-- Main script
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
-local whitelist = {
-    "tongguheren090325",
-    "Tongdscsh",
-    "shao_ba",
-}
-
-local customerlist = {
-
-}
+local whitelistScript = game:HttpGet("https://raw.githubusercontent.com/TongScriptX/Pluto/main/Pluto/whitelist.lua")
+local whitelist = loadstring(whitelistScript)()
 
 local player = game.Players.LocalPlayer
 local playerName = player.Name
@@ -33,17 +26,17 @@ end
 
 if isInCustomer then
     Notification:Notify(
-    {Title = "Pluto", Description = "检测到顾客名单,正在加载"},
-    {OutlineColor = Color3.fromRGB(209, 190, 169), Time = 5, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(209, 190, 169)}
+    {Title = "TX-BETA", Description = "检测到顾客名单,正在加载"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(74, 78, 105)}
 )
 end
 
 if isInWhitelist then
     Notification:Notify(
-    {Title = "Pluto", Description = "检测admin名单,正在加载"},
-    {OutlineColor = Color3.fromRGB(209, 190, 169), Time = 5, Type = "image"},
-    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(209, 190, 169)}
+    {Title = "TX-BETA", Description = "检测admin名单,正在加载"},
+    {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(74, 78, 105)}
     )
 end
 
@@ -54,14 +47,14 @@ if isInWhitelist or isInCustomer then
             local owner = player.Name
             if owner == "tongguheren090325" or owner == "Tongdscsh" then
                 Notification:Notify(
-                {Title = "Pluto", Description = "作者-Tong进入了服务器"},
-                {OutlineColor = Color3.fromRGB(209, 190, 169), Time = 5, Type = "image"},
-                {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(209, 190, 169)}
+                {Title = "TX", Description = "TX作者-Tong进入了服务器"},
+                {OutlineColor = Color3.fromRGB(80, 80, 80), Time = 5, Type = "image"},
+                {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(74, 78, 105)}
                 )
             end
         end
     )
-    print("hello pluto")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TongScriptX/TongScript/main/TXgames.lua"))()
 else
     game.Players.LocalPlayer:Kick("未检测到白名单")
 end
