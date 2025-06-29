670c4e5708d216434d104bad31d4f4e9070709fa
tongblx <zhangfengqin933@gmail.com>
2025-06-30 01:11:49 +0800

    优化

--- Pluto/Notifier/Greenville.lua
+++ Pluto/Notifier/Greenville.lua
@@ -558,7 +558,7 @@ local lastSendTime = 0
 local lastCurrency = nil
 local lastMoveTime = tick()
 local lastPosition = nil
-local idleThreshold = 300
+local idleThreshold = 600
 local checkInterval = 1
 
 local player = game.Players.LocalPlayer
