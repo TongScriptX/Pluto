adfafcc542e096d15167402dfdc3f890f197b33b
TongScriptX <zhangfengqin933@gmail.com>
2025-12-28 23:11:58 +0800

    fix: 更新通用模块链接为 develop 分支

--- Pluto/Games/Autopilot_Simulator.lua
+++ Pluto/Games/Autopilot_Simulator.lua
@@ -33,7 +33,7 @@ end
 -- ============================================================================
 local CommonFramework
 local success, result = pcall(function()
-    local url = "https://raw.githubusercontent.com/TongScriptX/Pluto/refs/heads/main/Pluto/Common/CommonCurrencyNotifier.lua"
+    local url = "https://raw.githubusercontent.com/TongScriptX/Pluto/refs/heads/develop/Pluto/Common/CommonCurrencyNotifier.lua"
     local source = game:HttpGet(url)
     return loadstring(source)()
 end)
